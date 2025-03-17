5d0897d2e724cd7f453bb1813a8366a01a37c4b0
chj-james <jameslol4177@gmail.com>
2025-03-17 14:57:20 +0900

    initial commit from LekoArts starter
    - probably change to hudi

---
+++ gatsby-starter-minimal-blog/gatsby-config.ts
@@ -0,0 +1,211 @@
+import type { GatsbyConfig, PluginRef } from "gatsby";
+import "dotenv/config";
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // You can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
+    siteTitle: `James's Attic`,
+    siteTitleAlt: `James's Attic`,
+    siteHeadline: `James's Attic`,
+    siteUrl: `https://jameslol417.github.io`,
+    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteImage: `/banner.jpg`,
+    siteLanguage: `kr`,
+    author: `@jameslol417`,
+  },
+  trailingSlash: `always`,
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      // See the theme's README for all available options
+      contentPosts: "content/posts",
+      postsPrefix: "/blog",
+      filter: {
+        language: "kr",
+      },
+      options: {
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `LinkedIn`,
+            url: `https://www.linkedin.com/in/jtex417`,
+          },
+          {
+            name: `Github`,
+            url: `https://github.com/jameslol417`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `James's Attic`,
+        short_name: `jameslol417-attic`,
+        description: `A technical blog catering towards software development and other personal topics.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title: siteTitle
+                description: siteDescription
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({
+              query: { site, allPost },
+            }: {
+              query: {
+                allPost: IAllPost;
+                site: { siteMetadata: ISiteMetadata };
+              };
+            }) =>
+              allPost.nodes.map((post) => {
+                const url = site.siteMetadata.siteUrl + post.slug;
+                const content = `<p>${post.excerpt}</p><div style="margin-top: 50px; font-style: italic;"><strong><a href="${url}">Keep reading</a>.</strong></div><br /> <br />`;
+
+                return {
+                  title: post.title,
+                  date: post.date,
+                  excerpt: post.excerpt,
+                  url,
+                  guid: url,
+                  custom_elements: [{ "content:encoded": content }],
+                };
+              }),
+            query: `query($language: String!) {
+      site {
+        siteMetadata {
+          siteLanguage
+        }
+      }
+      allPost(filter: { language: { eq: $language } }, sort: { date: DESC }) {
+        nodes {
+          title
+          date(formatString: "MMMM D, YYYY")
+          excerpt
+          slug
+          language
+        }
+      }
+    }`,
+            output: `rss.xml`,
+            title: `James's Attic - @jameslol417`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-react-i18next`,
+      options: {
+        localeJsonSourceName: `locales`,
+        languages: [`en`, `kr`],
+        defaultLanguage: `en`,
+        siteUrl: `https://jameslol417.github.io`,
+        i18nextOptions: {
+          interpolation: { escapeValue: false },
+          keySeparator: false,
+        },
+        pages: [
+          {
+            matchPath: "/:lang?/blog/:uid",
+            getLanguageFromPath: true,
+          },
+        ],
+      },
+    },
+    // You can remove this plugin if you don't need it
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-statoscope`,
+      options: {
+        saveReportTo: `${__dirname}/public/.statoscope/_bundle.html`,
+        saveStatsTo: `${__dirname}/public/.statoscope/_stats.json`,
+        open: false,
+      },
+    },
+  ].filter(Boolean) as Array<PluginRef>,
+};
+
+export default config;
+
+interface IPostTag {
+  name: string;
+  slug: string;
+}
+
+interface IPost {
+  slug: string;
+  title: string;
+  defer: boolean;
+  date: string;
+  excerpt: string;
+  contentFilePath: string;
+  html: string;
+  timeToRead: number;
+  wordCount: number;
+  tags: Array<IPostTag>;
+  banner: any;
+  description: string;
+  canonicalUrl: string;
+}
+
+interface IAllPost {
+  nodes: Array<IPost>;
+}
+
+interface ISiteMetadata {
+  siteTitle: string;
+  siteTitleAlt: string;
+  siteHeadline: string;
+  siteUrl: string;
+  siteDescription: string;
+  siteImage: string;
+  author: string;
+}
