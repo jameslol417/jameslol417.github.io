ec75e914577b302a869bb319f9dc4544807494b8
chj-james <jameslol4177@gmail.com>
2025-03-19 17:59:04 +0900

    feat: switched ot hoodie

---
+++ my-hoodie-blog/blog-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  title: "hoodie",
+  description: "Hello, I'm James who loves open source code and sharing knowledge.",
+  author: "jameslol417",
+  siteUrl: "https://jameslol417.github.io/",
+  links: {
+    github: "https://github.com/jameslol417",
+    linkedIn: "www.linkedin.com/in/jtex417",
+    email: "jtex417@ucsd.edu",
+    // resume: "https://hudi.blog",
+    // link: "https://hudi.blog",
+  },
+  useAbout: true,
+  // See https://giscus.app/
+  giscus: {
+    repo: "jameslol417/jameslol417.github.io",
+    repoId: "R_kgDOOJu7BA",
+    category: "Comments",
+    categoryId: "DIC_kwDOOJu7BM4CoNfj",
+    mapping: "pathname",
+    strict: "0",
+    reactionsEnabled: "1",
+    inputPosition: "bottom",
+    lang: "en",
+  },
+}
